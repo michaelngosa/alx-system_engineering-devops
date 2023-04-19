aef5688b0e7c7d088a2bb372227a5a9bbdfb892e
Michael ngosa <97114460+michaelngosa@users.noreply.github.com>
2023-04-19 14:36:54 +0200

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,7 +0,0 @@
-# installs puppet-lint package
-
-package {'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# kills a process called `killmenow`
-
-exec {'pkill -f killmenow':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
-}
